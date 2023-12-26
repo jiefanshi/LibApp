46cb6e70fe88cad15401394ae597ec569062d67a
wfei <wfei@pcienergysolutions.com>
2023-12-26 14:16:32 -0600

    initial commit, most frontend and backend frame are set up.

---
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Book.java
@@ -0,0 +1,40 @@
+package com.luv2code.springbootlibrary.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+//@Table(name = "book")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private  String title;
+
+    private String author;
+
+    private String description;
+
+    private int copies;
+
+    private int copiesAvailable;
+
+    private String category;
+
+    private String img;
+
+}
